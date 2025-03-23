5dc83ff2123abf561408d861e91b8af41f859d4b
Richard Liang <152163506+fearlesslyh@users.noreply.github.com>
2025-03-23 22:45:27 +0800

    feat(model): 新增枚举类
    - 添加 AppEnum 枚举类，用于定义得分类型和测试结果
    - 添加 ReviewStatusEnum枚举类，用于定义审核状态
    - 添加 ReviewStrategyEnum 枚举类，用于定义审核策略- 每个枚举类都提供了根据值获取枚举和获取所有值列表的方法

---
+++ TiDuoDuo-backend/src/main/java/com/lyh/TiDuoDuo/model/enums/AppEnum.java
@@ -0,0 +1,54 @@
+package com.lyh.TiDuoDuo.model.enums;
+
+import cn.hutool.core.util.ObjectUtil;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author <a href=https://github.com/fearlesslyh> 梁懿豪 </a>
+ * @version 1.0
+ * @date 2025/3/23 21:16
+ */
+public enum AppEnum {
+    SCORE("得分类", 0),
+    TEST("通过", 1);
+
+    private final String text;
+
+    private final int value;
+
+    AppEnum(String text, int value) {
+        this.text = text;
+        this.value = value;
+    }
+
+    /**
+     * 根据 value 获取枚举
+     *
+     * @param value
+     * @return
+     */
+    public static AppEnum getEnumByValue(Integer value) {
+        if (ObjectUtil.isEmpty(value)) {
+            return null;
+        }
+        for (AppEnum anEnum : AppEnum.values()) {
+            if (anEnum.value == value) {
+                return anEnum;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 获取值列表
+     *
+     * @return
+     */
+    public static List<Integer> getValues() {
+        return Arrays.stream(values()).map(item -> item.value).collect(Collectors.toList());
+    }
+
+}
---
+++ TiDuoDuo-backend/src/main/java/com/lyh/TiDuoDuo/model/enums/ReviewStrategyEnum.java
@@ -0,0 +1,54 @@
+package com.lyh.TiDuoDuo.model.enums;
+
+import cn.hutool.core.util.ObjectUtil;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author <a href=https://github.com/fearlesslyh> 梁懿豪 </a>
+ * @version 1.0
+ * @date 2025/3/23 21:16
+ */
+public enum ReviewStrategyEnum {
+    CUSTOMIZE("自定义", 0),
+    AI("AI", 1);
+
+    private final String text;
+
+    private final int value;
+
+    ReviewStrategyEnum(String text, int value) {
+        this.text = text;
+        this.value = value;
+    }
+
+    /**
+     * 根据 value 获取枚举
+     *
+     * @param value
+     * @return
+     */
+    public static ReviewStrategyEnum getEnumByValue(Integer value) {
+        if (ObjectUtil.isEmpty(value)) {
+            return null;
+        }
+        for (ReviewStrategyEnum anEnum : ReviewStrategyEnum.values()) {
+            if (anEnum.value == value) {
+                return anEnum;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 获取值列表
+     *
+     * @return
+     */
+    public static List<Integer> getValues() {
+        return Arrays.stream(values()).map(item -> item.value).collect(Collectors.toList());
+    }
+
+}
